a98da5587f53cea0752c2c8fa277234570b09c6e
vinicius <vinycastro1@gmail.com>
2023-06-03 09:22:01 -0300

    atualização de imagens

--- js/script.js
+++ js/script.js
@@ -1,76 +1,62 @@
-document.getElementById("botaoEnviar").addEventListener("click", function (event){
-  event.preventDefault()
-  validaFormulario()
-})
+const meuBotao = document.getElementById("botaoEnviar");
+let nomeCampo;
+let emailCampo;
+let telefoneCampo;
+let sugestaoCampo;
 
-function stringEVazia(texto){
-  return texto == ""
+function clickBotao(evento) {
+  evento.preventDefault();
+  console.log("Botão Clicado");
+  pegaInputs();
+  validaFormulario();
 }
 
-function validaFormulario(){
+function stringEVazia(texto) {
+  return texto === "";
+}
 
-  const nomeEVazio =  stringEVazia(document.getElementById("nome").value);
-  const emailEVazio =  stringEVazia(document.getElementById("email").value);
-  const telefoneEVazio =  stringEVazia(document.getElementById("telefone").value);
+function pegaInputs() {
+  nomeCampo = document.getElementById("nome").value;
+  emailCampo = document.getElementById("email").value;
+  telefoneCampo = document.getElementById("telefone").value;
+  sugestaoCampo = document.getElementById("sugestao").value;
+  console.log(nomeCampo, emailCampo, telefoneCampo, sugestaoCampo);
+  console.log(stringEVazia(nomeCampo));
+}
 
-  if(nomeEVazio === true){ // pode ser só if(nomeEVazio)
-    alert("Preencha o campo nome")
+meuBotao.addEventListener("click", clickBotao);
+
+function validaFormulario() {
+  const nomeEVazio = stringEVazia(nomeCampo);
+  const emailEVazio = stringEVazia(emailCampo);
+  const telefoneEVazio = stringEVazia(telefoneCampo);
+
+  if (nomeEVazio === true) {
+    alert("Preencha o campo nome");
     return;
-  } else if(emailEVazio === true){
-    alert("Preencha o campo email")
+  } else if (emailEVazio === true) {
+    alert("Preencha o campo email");
     return;
-  }if(telefoneEVazio === true){
-    alert("Preencha o campo telefone")
+  }
+  if (telefoneEVazio === true) {
+    alert("Preencha o campo telefone");
     return;
-  }else{
-    console.log("Formulário Enviado")
+  } else {
+    console.log("Formulário Enviado");
   }
 }
 
-function criarItemDeGaleria(src, i){
-  const imagem = document.createElement('img');
-  imagem.setAttribute('src', src)
-  imagem.setAttribute('alt', 'nina'+i)
+const galeria = document.getElementById("galeria-principal");
 
-  return imagem
+let imagensGaleria = [
+  "https://raw.githubusercontent.com/camilapessoa/jovem-aprendiz/master/img/ana-carolina-da-hora-hackear-o-racismo-1594591816588_v2_1x1.jpg",
+  "https://raw.githubusercontent.com/camilapessoa/jovem-aprendiz/master/img/unnamed.jpg",
+  "https://raw.githubusercontent.com/camilapessoa/jovem-aprendiz/master/img/aoNiv4uZ_400x400.jpg",
+  "https://raw.githubusercontent.com/camilapessoa/jovem-aprendiz/master/img/nina-da-hora-30052022-150531.png"
+];
 
+for (let i = 0; i < imagensGaleria.length; i = i + 1) {
+  const imagem = document.createElement("img");
+  imagem.src = imagensGaleria[i];
+  galeria.appendChild(imagem);
 }
-function criarQuebraDeLinha(){
-  const myImage = document.createElement('br');
-  return myImage
-}
-
-function numeroPar(numero){
-  return numero % 2 === 0 ? true: false
-}
-
-function criarGaleria(){
-
-  // Conceito de array e objetos
-  const imagensGaleria=[
-    'img/nina.jpeg',
-    'img/nina2.png',
-    'img/nina3.jpg',
-    'img/nina4.jpg',
-  ]
-
-  const galeria = document.querySelector("#galeria-principal")
-
-  for(let i = 0 ; i < imagensGaleria.length; i++){
-    const item = imagensGaleria[i];
-    const imagem = criarItemDeGaleria(item, i)
-
-    if(numeroPar(i)){
-      const quebraDelinha = criarQuebraDeLinha()
-      galeria.appendChild(quebraDelinha)
-    }
-
-    galeria.appendChild(imagem)
-  }
-
-}
-
-criarGaleria()
-
-// Passo 1: Selecionar um elemento
-
